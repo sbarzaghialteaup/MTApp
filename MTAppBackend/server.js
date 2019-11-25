dec921f6bc1372146bf0aacb4ae2a82398adedfc
Samuele Barzaghi <sbarzaghi@alteanet.it>
2019-11-25 18:09:42 +0100

    Ping request without access to db

--- MTAppBackend/server.js
+++ MTAppBackend/server.js
@@ -126,6 +126,15 @@ app.get("/get_legal_entity", function (req, res) {
 	});
 });
 
+app.get("/ping", function (req, res) {
+
+	var responseStr = "";
+	responseStr += "<!DOCTYPE HTML><html><head><title>MTApp</title></head><body><h1>MTApp</h1><h2>Welcome " + req.authInfo.userInfo.givenName + " " + req.authInfo.userInfo.familyName + "!</h2><p><b>Subdomain:</b> " + req.authInfo.subdomain + "</p><br />";
+	responseStr += "<p><b>Identity Zone:</b> " + req.authInfo.identityZone + "</p><p><b>Origin:</b> " + req.authInfo.origin + "</p>" + "<br /><br /><pre>" + "Hello, I'm there!!!" + "</pre>" + "</body></html>";
+	res.status(200).send(responseStr);
+	
+});
+
 app.get("/add_legal_entity", function (req, res) {
 	var reqStr = stringifyObj(req.authInfo.userInfo, {
 		indent: "   ",
