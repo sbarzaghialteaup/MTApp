b1eb0319edd611cdf96383cacca5ded925cb3a0d
Samuele Barzaghi <sbarzaghi@alteanet.it>
2019-12-12 22:49:19 +0100

    Comandi per fare il push dell'app e il deploy nel portale, il deploy serve però solo quando si aggiunge un app nel launchpad

--- Launchpad/alteaup_deploy_to_portal.sh
+++ Launchpad/alteaup_deploy_to_portal.sh
@@ -1,1 +1,2 @@
+cd /home/vcap/app/
 ~/deps/0/node/bin/node  --harmony node_modules/@sap/portal-cf-content-deployer/src/index.js
--- html5appdeployer/resources/push.sh
+++ html5appdeployer/resources/push.sh
@@ -1,3 +1,5 @@
 date 
 cf html5-push -n html5-repo
 date
+cf ssh launchpad-deployer -c '/home/vcap/app/alteaup_deploy_to_portal.sh'
+date
